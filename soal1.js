275dcb8c72822c705a4f42dda42da91f9cba7480
Dicky Anugrah <dickyangrh@gmail.com>
2024-02-17 15:08:05 +0700

    tugas javascript

---
+++ soal1.js
@@ -0,0 +1,20 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  readline.question('Masukkan bilangan genap positif: ', (input) => {
+    const x = parseInt(input);
+  
+    if (x < 0) {
+      console.log('Tidak bisa input bilangan negatif');
+    } else if (x % 2 !== 0) {
+      console.log('Tidak bisa input bilangan ganjil');
+    } else {
+      const result = Math.sqrt(x);
+      console.log(`Akar pangkat 2 dari ${x} adalah ${result}`);
+    }
+  
+    readline.close();
+  });
+  
